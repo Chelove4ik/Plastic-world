c4ebf41d51e3554c9d0a2d69299d4fc9240027ee
pavelru <pahaprimorye2015@gmail.com>
2019-10-04 15:37:10 +1000

    all

---
+++ sber.py
@@ -0,0 +1,10 @@
+class Employee:
+    def __init__(self, name, type, weekends=[], res_days=[], priority_shift=[], holidays=[]):
+        self.name = name
+        self.type = type
+        self.weekens = weekends
+        self.res_days = res_days
+        self.priority_shift = priority_shift
+        self.holidays = holidays
+
+
